49a8cb9a511870c0bbc62426c8c5bf4d760d623d
Ashwin Wadte <ashwinwadte@gmail.com>
2020-04-23 13:50:17 +0530

    feat: Classify the light state using classifier

--- ros/src/tl_detector/tl_detector.py
+++ ros/src/tl_detector/tl_detector.py
@@ -133,15 +133,17 @@ class TLDetector(object):
             int: ID of traffic light color (specified in styx_msgs/TrafficLight)
 
         """
-        # if(not self.has_image):
-        #     self.prev_light_loc = None
-        #     return False
+        if(not self.has_image):
+            self.prev_light_loc = None
+            return False
 
-        # cv_image = self.bridge.imgmsg_to_cv2(self.camera_image, "bgr8")
+        cv_image = self.bridge.imgmsg_to_cv2(self.camera_image, "bgr8")
 
-        # #Get classification
-        # return self.light_classifier.get_classification(cv_image)
-        return light.state
+        #Get classification
+        classification_result = self.light_classifier.get_classification(cv_image)
+        rospy.loginfo("ground truth: %s, classifier result: %s", light.state, classification_result)
+        return classification_result
+#         return light.state
 
     def process_traffic_lights(self):
         """Finds closest visible traffic light, if one exists, and determines its
